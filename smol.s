325e5d6b4e446c2d7c25c93ce368bee06ac1510f
llwyd <tom@llwyd.io>
2021-05-07 18:37:54 +0100

    this blinks when stepping through, but no delay yet

--- smol.s
+++ smol.s
@@ -5,13 +5,26 @@
     .globl _start
 
 _start:
-    .word 0x20001000
+    /* This is where the stack top goes, but we dont
+    * need a stack for this so going to repurpose it*/
+    .word 0x4002104C
+    /* Second word is always the reset vector */
     .word _reset
 
 .globl _reset
 .thumb_func
 _reset:
-    
+    mov r0, 0x2
+    ldr r1, = _stext
+    ldr r2, [r1]
+    str r0, [r2]
+
+    ldr r0, = 0x48000400
+    /* Reuse the number stored in r2, as it has 
+    * the right bits set for PB3 */ 
+    str r2, [r0, #0x00]
 loop:
+    eor r3, #0x8
+    str r3, [r0, #0x14]
     b loop
 
